7b4cb57fe02aaf531054d1f1b43881dd73b80c2e
Jason Reeves <jasonreeves@MWI-jasonreeves-MRXCYVQW24.local>
2025-12-16 05:35:54 -0600

    more formatting / cleanup

--- lib/sayings.rb
+++ lib/sayings.rb
@@ -2,30 +2,30 @@ module Sayings
 
   def Sayings.get_saying(type)
     happy = [
-       "More ramen to you!",
-       "I'm pleased.",
-       "I see great potential.",
-       "Thou shalt pass.",
-       "Good, good!",
-       "That was quite something.",
-       "Most excellent.",
-       "I'm proud of you.",
-       "Your brothers are proud of you.",
-       "Your sisters are proud of you.",
-       "Like a feather, your code floats in the air.",
-       "Pizza for you! With anchovies I presume?",
-       "Sweet smell of working code.",
-       "As you grow older, you will learn there are many ways to do the right thing.",
-       "Miyagi have hope for you.",
-       "Good! Now use head for something other than target.",
-       "Man who catch fly with chopstick accomplish anything.",
-       "Banzai!",
-       "Do you want to learn kung fu?",
-       "I vowed to train you, and you have been trained. You are free to eat.",
-       "There are no accidents.",
-       "What goes on inside your head I do not always understand. But what goes on in your heart will never let us down.",
-       "Beautiful. Elegant. Perfect. You made me proud.",
-       "The Force will be with you, always."
+      "More ramen to you!",
+      "I'm pleased.",
+      "I see great potential.",
+      "Thou shalt pass.",
+      "Good, good!",
+      "That was quite something.",
+      "Most excellent.",
+      "I'm proud of you.",
+      "Your brothers are proud of you.",
+      "Your sisters are proud of you.",
+      "Like a feather, your code floats in the air.",
+      "Pizza for you! With anchovies I presume?",
+      "Sweet smell of working code.",
+      "As you grow older, you will learn there are many ways to do the right thing.",
+      "Miyagi have hope for you.",
+      "Good! Now use head for something other than target.",
+      "Man who catch fly with chopstick accomplish anything.",
+      "Banzai!",
+      "Do you want to learn kung fu?",
+      "I vowed to train you, and you have been trained. You are free to eat.",
+      "There are no accidents.",
+      "What goes on inside your head I do not always understand. But what goes on in your heart will never let us down.",
+      "Beautiful. Elegant. Perfect. You made me proud.",
+      "The Force will be with you, always."
     ]
               
     sad = [
@@ -57,10 +57,10 @@ module Sayings
        "To make a bad day worse spend it wishing for the impossible."
     ]
 
-	saying = sad.sample
-	if type == "happy"
-        saying = happy.sample
-	end
-	saying
+    saying = sad.sample
+    if type == "happy"
+      saying = happy.sample
+    end
+    saying
   end
 end
