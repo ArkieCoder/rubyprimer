c9384bd45b0b3f6269fe7a02dbc8c4610bbd5ec7
Jason Reeves <jasonreeves@MWI-jasonreeves-MRXCYVQW24.local>
2025-12-19 13:23:23 -0600

    add lesson 4.0

---
+++ examples/4.0.1.rb
@@ -0,0 +1,6 @@
+restaurant_menu = {
+  "Ramen" => 3,
+  "Dal Makhani" => 4,
+  "Tea" => 2
+}
+## how do you use restaurant_menu to get the price of Ramen?
---
+++ examples_spec/4.0.1.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+
+describe "example code" do
+  let(:code) { File.read('__TMPFILE__') }
+  let(:result) { eval(code) }
+
+  it "should return 3" do
+    expect(result).to eq(3)
+  end
+end
